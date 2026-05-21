a5638eeb84c1e608f23218271b27d31eae8e097e
Muhammad-Sarfaraz-Ahmed <ahmedsarfrazsafee@gmail.com>
2026-05-21 15:56:32 +0500

    Improved Book class and updated book records

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import json
 
 
 class Book:
+
     def __init__(self, title, author):
+
         self.title = title
         self.author = author
+
+    def __str__(self):
+
+        return f"{self.title} by {self.author}"
+
+    def to_dict(self):
+
+        return {
+            "title": self.title,
+            "author": self.author
+        }
 
     def display_book(self):
         print(f"Title: {self.title}")
